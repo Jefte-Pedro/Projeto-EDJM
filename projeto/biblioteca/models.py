199738b6926bd06b94598189923ff7af0f706541
Luiz Aelxandre <luizale2019@gmail.com>
2026-03-14 13:21:57 -0300

    introdução do backend do projeto

---
+++ projeto/biblioteca/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Livro(models.Model):
+    titulo = models.CharField(max_length=200)
+    autor = models.CharField(max_length=150)
+    editora = models.CharField(max_length=150)
+    genero =  models.CharField(max_length=100)
+    quantidade = models.IntegerField()
+
+    def __str__(self):
+        return self.titulo
